5b815fced74e2c1cdc57928e6694908b22babe45
sreenivas pallapu <sreenivas.engece@gmail.com>
2015-04-15 15:43:59 +0530

    completed basic structure with copyright symbol

--- eminer/views.py
+++ eminer/views.py
@@ -1,22 +1,19 @@
 from django.shortcuts import render
-from eminer.models import NewEntry
+from eminer.models import NewEntry, Tag
 from django.http import HttpResponse
 # Create your views here.
 
-def BlogView(request, post_name=None, count=0):
-    if post_name == None:
+def BlogView(request, post_name=None, count=0, tag=None):
+    recent = NewEntry.objects.order_by('-posted')[:5]
+    if post_name:
+        posts = NewEntry.objects.filter(slug=post_name).values()
+    elif tag:
+        tags = Tag.objects.get(slug=tag)
+        posts = NewEntry.objects.filter(tag = tags)
+    else:
         count = int(count)
         posts = NewEntry.objects.order_by('-posted')[count:count+2]
         count += 2
-        """try:
-            posts = NewEntry.objects.order_by('-posted')[count:count+5]
-            count = count + 5
-        except DoesNotExist:
-            posts = NewEntry.objects.order_by('-posted')[:5]"""
-        val = 0
-    else:
-        posts = NewEntry.objects.get(slug=post_name)
-        val = 1
-    return render(request, 'eminer/index.html', {'posts':posts, 'val':val, 'count':count})
+    return render(request, 'eminer/index.html', {'posts':posts, 'count':count, 'recent':recent})
 def About(request):
     return render(request, 'eminer/about.html')
